f4a70be9b0e28928f4386f993422162ef89f0b90
DurandA <arnaud.durand@unifr.ch>
2018-06-06 18:27:58 +0200

    Discover services and characteristics
    See https://googlechrome.github.io/samples/web-bluetooth/discover-services-and-characteristics.html

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
-let chosenHeartRateService = null;
-
 button = document.getElementById("ble-scan");
 
 button.addEventListener('pointerup', function(event) {
   navigator.bluetooth.requestDevice({
     filters: [{ services: ['ef680100-9b35-4933-9b10-52ffa9740042'] }],
-    optionalServices: ['ef680200-9b35-4933-9b10-52ffa9740042']
-  }).then(device => device.gatt.connect())
-  .then(server => server.
-                        getPrimaryService(
-      'ef680200-9b35-4933-9b10-52ffa9740042'))
-  .then(service => {
-    thingyEnvironmentService = service;
-    return Promise.all([
-      service.
-                 getCharacteristic(
-      'ef680201-9b35-4933-9b10-52ffa9740042')
-        .then(console.log),
-      service.
-                 getCharacteristic(
-      'ef680203-9b35-4933-9b10-52ffa9740042')
-        .then(console.log),
-    ]);
+    //acceptAllDevices: true,
+    optionalServices: [
+      'ef680200-9b35-4933-9b10-52ffa9740042',
+      'ef680300-9b35-4933-9b10-52ffa9740042',
+    ]
+  }).then(device => {
+    console.log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    // Note that we could also get all services that match a specific UUID by
+    // passing it to getPrimaryServices().
+    console.log('Getting Services...');
+    return server.getPrimaryServices();
+  })
+  .then(services => {
+    console.log('Getting Characteristics...');
+    let queue = Promise.resolve();
+    services.forEach(service => {
+      queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+        console.log('> Service: ' + service.uuid);
+        characteristics.forEach(characteristic => {
+          console.log('>> Characteristic: ' + characteristic.uuid + ' ' +
+              getSupportedProperties(characteristic));
+        });
+      }));
+    });
+    return queue;
+  })
+  .catch(error => {
+    console.log('Argh! ' + error);
   });
 });
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
+}
