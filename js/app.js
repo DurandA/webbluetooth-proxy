51f68d6ec33ad417c2a9f3834f6d00d36ce24a51
DurandA <arnaud.durand@unifr.ch>
2018-06-11 22:56:02 +0200

    Add services view

--- js/app.js
+++ js/app.js
@@ -1,6 +1,61 @@
 button = document.getElementById("ble-scan");
 deviceTable = document.getElementById("device-table");
 
+function createSwitch(root){
+  let label = Object.assign(document.createElement("label"), {className:'switch'});
+  let input = Object.assign(document.createElement("input"), {type:'checkbox'});
+  let span = Object.assign(document.createElement("span"), {className:'slider round'});
+
+  label.appendChild(input)
+  label.appendChild(span)
+  root.appendChild(label)
+
+  return input
+}
+
+function createDeviceElement(deviceId, characteristics){
+  let container = Object.assign(document.createElement("div"), {className:'tabcontent'});
+  container.id = 'dev-'+deviceId;
+  let sUl = document.createElement("UL");
+  container.appendChild(sUl);
+  for (var service in characteristics) {
+    let sLi = document.createElement("LI");
+    sLi.appendChild(document.createTextNode(service));
+    sUl.appendChild(sLi);
+    let cUl = document.createElement("UL");
+    sUl.appendChild(cUl);
+    characteristics[service].forEach(characteristic => {
+      let cLi = document.createElement("LI");
+      cLi.appendChild(document.createTextNode(characteristic.uuid));
+
+      let input = Object.assign(document.createElement("input"),{
+        type:'checkbox', checked:characteristic.properties.read, disabled:true});
+      let label = Object.assign(document.createElement("label"), {className:'label-inline'});
+      label.appendChild(input);
+      label.appendChild(document.createTextNode("read"));
+      cLi.appendChild(label);
+
+      input = Object.assign(document.createElement("input"),{
+        type:'checkbox', checked:characteristic.properties.write, disabled:true});
+      label = Object.assign(document.createElement("label"), {className:'label-inline'});
+      label.appendChild(input);
+      label.appendChild(document.createTextNode("write"));
+      cLi.appendChild(label);
+
+      input = Object.assign(document.createElement("input"),{
+        type:'checkbox', checked:false, disabled:!characteristic.properties.notify});
+      input.onclick = handleCharacteristic.bind(null, characteristic)
+      label = Object.assign(document.createElement("label"), {className:'label-inline'});
+      label.appendChild(input);
+      label.appendChild(document.createTextNode("notify"));
+      cLi.appendChild(label);
+
+      cUl.appendChild(cLi);
+    });
+  }
+  return container;
+}
+
 button.addEventListener('pointerup', function(event) {
   navigator.bluetooth.requestDevice({
     filters: [{ services: ['ef680100-9b35-4933-9b10-52ffa9740042'] }],
@@ -13,14 +68,15 @@ button.addEventListener('pointerup', function(event) {
     console.log(device);
     let row = deviceTable.insertRow(-1);
     let name = row.insertCell(0);
+    let tab = Object.assign(document.createElement("button"), {id:device.id, className:'tablinks button-outline'});
+    tab.appendChild(document.createTextNode(device.name));
+    tab.onclick = openDevice.bind(null, device.id)
     let id = row.insertCell(1);
 
-    name.innerHTML = device.name;
+    name.appendChild(tab);
     id.innerHTML = device.id;
 
-    let connect = document.createElement('INPUT');
-    connect.type = 'checkbox';
-    row.insertCell(2).appendChild(connect);
+    let connect = createSwitch(row.insertCell(2))
 
     let connected = row.insertCell(3);
     connected.innerHTML = device.gatt.connected;
@@ -30,28 +86,7 @@ button.addEventListener('pointerup', function(event) {
       if (connect.checked) {
         device.gatt.connect().then(server => {
           getCharacteristics(device).then(characteristics => {
-            let charList = row.insertCell(4)
-            let sUl = document.createElement("UL");
-            charList.appendChild(sUl);
-            for (var service in characteristics) {
-              let sLi = document.createElement("LI");
-              sLi.appendChild(document.createTextNode(service));
-              sUl.appendChild(sLi);
-              let cUl = document.createElement("UL");
-              sUl.appendChild(cUl);
-              characteristics[service].forEach(characteristic => {
-                let cLi = document.createElement("LI");
-                cLi.appendChild(document.createTextNode(characteristic.uuid));
-                if (characteristic.properties.notify) {
-                  let notifyBtn = document.createElement("BUTTON");
-                  notifyBtn.appendChild(document.createTextNode("Notify"));
-                  notifyBtn.onclick = handleCharacteristic.bind(null, characteristic)
-                  cLi.appendChild(notifyBtn);
-                }
-                cUl.appendChild(cLi);
-                console.log(characteristic);
-              });
-            }
+            document.getElementById('device').appendChild(createDeviceElement(device.id, characteristics));
           });
           connected.innerHTML = device.gatt.connected;
           connect.disabled = false;
@@ -86,8 +121,33 @@ function getCharacteristics(device){
 
 function handleCharacteristic(characteristic) {
   return characteristic.startNotifications()
-  .then(char => {
-    characteristic.addEventListener('characteristicvaluechanged',
-                                    event => console.log(event));
+  .then(characteristic => {
+    characteristic.addEventListener('characteristicvaluechanged', e => {
+      const view = e.target.value;
+    });
   });
+}
+
+// https://www.w3schools.com/howto/howto_js_tabs.asp
+function openDevice(deviceId, evt) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.add("button-outline");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    let container = document.getElementById('dev-'+deviceId);
+    if (container) {
+      container.style.display = "block";
+    }
+    evt.currentTarget.classList.remove("button-outline");
 }
